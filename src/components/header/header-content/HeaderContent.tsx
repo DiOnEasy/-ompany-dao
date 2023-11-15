da5e293cd107db0e80d3f2afc7fe4c03fd70d2d1
Денис Стасишен <d11062002d@gmail.com>
2023-11-15 12:56:01 +0200

    header content styled

--- src/components/header/header-content/HeaderContent.tsx
+++ src/components/header/header-content/HeaderContent.tsx
@@ -1,15 +1,18 @@
+import s from './HeaderContent.module.css';
+
+
 export const HeaderContent = () => {
   return (
     <>
-      <div>
-        <div>
+      <div className={s.wrapper}>
+        <div className={s.title}>
           <p>Tokenize any business or asset</p>
         </div>
-        <div>
+        <div className={s.text}>
           Access capital, unlock liquidity, enable fractional ownership and grow
           your business with token incentives
         </div>
-        <div>
+        <div className={s.button}>
           <a href="">
             <span>Enter App</span>
             <span><img src="/img/arrow-right.svg" alt="" /></span>
