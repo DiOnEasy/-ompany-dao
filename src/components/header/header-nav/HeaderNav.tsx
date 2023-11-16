7713bc668e6ea24662d8e3f922ffe0ab796905da
Денис Стасишен <d11062002d@gmail.com>
2023-11-17 01:34:33 +0200

    created mobile menu

--- src/components/header/header-nav/HeaderNav.tsx
+++ src/components/header/header-nav/HeaderNav.tsx
@@ -1,28 +1,83 @@
-import s from './HeaderNav.module.css';
-
-
-
+import { useEffect, useState } from "react";
+import s from "./HeaderNav.module.css";
 
 export const HeaderNav = () => {
-    return (
-      // <div className={s.header}>
-          <div className={s.wrapper}>
-          <div className={s.logo}>
-            <p>Company</p>
-            <img src="/img/logo.svg" alt="" />
-          </div>
-          <div className={s.navbar}>
-            <a href=""><img src="/img/youtube.svg" alt="" />Watch Demo</a>
+  const [isTablet, setIsTablet] = useState(true);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsTablet(true);
+      } else {
+        setIsTablet(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    // <div className={s.header}>
+    <div className={s.wrapper}>
+      <div className={s.logo}>
+        <p>Company</p>
+        <img src="/img/logo.svg" alt="" />
+      </div>
+      <div className={s.navbar}>
+        {!isTablet && (
+          <>
+            <a href="">
+              <img src="/img/youtube.svg" alt="" />
+              Watch Demo
+            </a>
             <a href="">Telegram</a>
             <a href="">Twitter</a>
             <a href="">Medium</a>
             <a href="">Get in touch</a>
-            <a href="">Enter App <img src="/img/small-arrow-right.svg" alt="" /></a>
-          </div>
-        </div>
-      
-      // </div>
-      
-    );
-  };
-  
+          </>
+        )}
+
+        <a className={s.navbar__button} href="">
+          Enter App <img src="/img/small-arrow-right.svg" alt="" />
+        </a>
+        {isTablet && (
+          <>
+            <div
+              onClick={() => setMobileMenu(!mobileMenu)}
+              className={s.burger}
+            >
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div
+              className={`${s.mobile__menu} ${
+                mobileMenu == true ? s.active : ""
+              }`}
+            >
+              <a onClick={() => setMobileMenu(!mobileMenu)} href="">
+                <img src="/img/youtube.svg" alt="" />
+                Watch Demo
+              </a>
+              <a onClick={() => setMobileMenu(!mobileMenu)} href="">
+                Telegram
+              </a>
+              <a onClick={() => setMobileMenu(!mobileMenu)} href="">
+                Twitter
+              </a>
+              <a onClick={() => setMobileMenu(!mobileMenu)} href="">
+                Medium
+              </a>
+              <a onClick={() => setMobileMenu(!mobileMenu)} href="">
+                Get in touch
+              </a>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+
+    // </div>
+  );
+};
